b38ad195382a1234140283ed2bd5cd94934ebc30
jbeedle19 <josh.beedle@gmail.com>
2022-03-03 18:44:08 -0500

    added README, updated error messages to be alerts

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project 2: Commerce
+
+## Description
+This is the third project for CS50p, Web Programming with Python and JavaScript. Commerce is an example of an e-commerce application. A user is able to post auction listings, place bids on listings, comment on those listings, and add listings to a “watchlist.”
+
+## Built With
+* HTML, CSS, and JavaScript
+* Python
+* Django
--- commerce/settings.py
+++ commerce/settings.py
@@ -21,6 +21,8 @@ env = environ.Env(
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Read .env file
+environ.Env.read_env(env_file=str(BASE_DIR) + '/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
